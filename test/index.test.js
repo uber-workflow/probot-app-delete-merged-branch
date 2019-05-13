90b71733d7ed4dc374ec83ff2c7e11e201d11110
Chris Deacy <deacy@uber.com>
2019-05-13 08:51:52 -0700

    Upgrade probot (#5)

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,37 @@
-const { Application } = require('probot')
+const fs = require('fs')
+const path = require('path')
+const { Probot } = require('probot')
 const plugin = require('../index')
 const deleteMergedBranch = require('../lib/delete-merged-branch')
 const payload = require('./fixtures/pull-request.closed')
+
+const MOCK_CERT_PATH = path.resolve(__dirname, 'fixtures/mock-cert.pem')
+// mock cert copied from copied from:
+// https://github.com/probot/create-probot-app/blob/de9078d/templates/basic-js/test/fixtures/mock-cert.pem
+const MOCK_CERT = fs.readFileSync(MOCK_CERT_PATH, 'utf-8')
 
 jest.mock('../lib/delete-merged-branch', () => jest.fn())
 
 describe('Auto-delete-merged-branch ProBot Application', () => {
   let app
-  let github
 
   beforeEach(() => {
-    app = new Application()
+    app = new Probot({
+      id: 123,
+      cert: MOCK_CERT
+    })
+
     app.load(plugin)
-    app.auth = () => Promise.resolve(github)
   })
 
   describe('Delete branch functionality', () => {
     describe('It does not receive the `pull_request.closed` event', () => {
       beforeEach(async () => {
         const event = 'pull_request.open'
-        await app.receive({ event, payload })
+        await app.receive({ name: event, payload })
       })
 
-      it('should NOT call the deleteReference method', () => {
+      it('should NOT call the deleteRef method', () => {
         expect(deleteMergedBranch).not.toHaveBeenCalled()
       })
     })
@@ -30,10 +39,10 @@ describe('Auto-delete-merged-branch ProBot Application', () => {
     describe('It receives the `pull_request.closed` event', () => {
       beforeEach(async () => {
         const event = 'pull_request.closed'
-        await app.receive({ event, payload })
+        await app.receive({ name: event, payload })
       })
 
-      it('should call the deleteReference method', () => {
+      it('should call the deleteRef method', () => {
         expect(deleteMergedBranch).toHaveBeenCalled()
       })
     })
